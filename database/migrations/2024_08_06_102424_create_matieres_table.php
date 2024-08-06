83510124590752cf54fd4bf2987270d53124bf0c
SerigneFallouNiang <fallouniang776@gmail.com>
2024-08-06 11:01:33 +0000

    configuration des tables

---
+++ database/migrations/2024_08_06_102424_create_matieres_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('matieres', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Matiere::class)->constrained()->onDelete('cascade');
+            $table->string('libelle');
+            $table->string('date_debut');
+            $table->string('date_fin');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('matieres');
+    }
+};
---
+++ database/migrations/2024_08_06_102452_create_evaluations_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('evaluations', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Etudiant::class)->onDelete('cascade');
+            $table->foreignIdFor(Matiere::class)->onDelete('cascade');
+            $table->date('date');
+            $table->integer('valeur');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('evaluations');
+    }
+};
